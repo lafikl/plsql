720887f61c97c9211612e26fa3d60e6d39b06ada
Khalid Lafi <lafi.khalid@gmail.com>
2018-10-21 16:23:17 +0300

    add all the things

---
+++ listner.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"github.com/lafikl/prsr/sqllexer"
+)
+
+func main() {
+	// Setup the input
+	is := antlr.NewInputStream("1 + 2 * 3")
+
+	// Create the Lexer
+	lexer := parser.NewPlSqlLexer(is)
+
+	// Read all tokens
+	for {
+		t := lexer.NextToken()
+		if t.GetTokenType() == antlr.TokenEOF {
+			break
+		}
+		fmt.Printf("%s (%q)\n",
+			lexer.SymbolicNames[t.GetTokenType()], t.GetText())
+	}
+}
